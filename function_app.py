2ebdbf379d804dc5a1530f2d1a319c434ebd8cc5
David Leyden <david.leyden@serco-ap.com>
2026-03-10 14:33:24 +1100

    fix function_app for durable

--- function_app.py
+++ function_app.py
@@ -1,3 +1,4 @@
+import azure.durable_functions as df
 import azure.functions as func
 from src.ocds_routes import ocds_blueprint
 from src.ocds_durable import ocds_durable_blueprint
@@ -5,7 +6,7 @@ from src.connectivity_routes import connectivity_blueprint
 from src.timer_function import timer_blueprint
 from src.table_storage_routes import table_storage_blueprint
 
-app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
+app = df.DFApp(http_auth_level=func.AuthLevel.FUNCTION)
 app.register_functions(ocds_blueprint)
 app.register_functions(ocds_durable_blueprint)
 app.register_functions(connectivity_blueprint)
